bcc594fba94f146d2c3667362e2a308ae886047a
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-08-25 11:37:57 +0200

    activesupport

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,6 @@
+require 'active_support'
+require 'active_support/core_ext'
+#
 # Defines our constants
 RACK_ENV = ENV['RACK_ENV'] ||= 'development'  unless defined?(RACK_ENV)
 PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
